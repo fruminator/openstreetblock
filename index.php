0cd25a3dc3f7527bbdc66b3f4c7056963b811499
mfrumin <mfrumin@barelyconnected.net>
2011-03-19 15:29:24 -0700

    intro page

---
+++ index.php
@@ -0,0 +1,6 @@
+holdover introductory page.  
+
+Try <a href="osb.php?ll=40.6231433,-74.0282185">osb.php?ll=40.6231433,-74.0282185</a>
+<BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=json">osb.php?ll=40.6231433,-74.0282185&format=json</a>
+<BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=debug">osb.php?ll=40.6231433,-74.0282185&format=debug</a>
+<BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=rawdata">osb.php?ll=40.6231433,-74.0282185&format=rawdata</a>
