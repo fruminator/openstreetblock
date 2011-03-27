b41331b7dbf25ed6be36a8fb2cd078238f56008c
fruminator <michael@frumin.net>
2011-03-27 13:38:00 -0700

    put in apache license everywhere

--- debug.tmpl.php
+++ debug.tmpl.php
@@ -1,6 +1,33 @@
+<?php
+
+  /**
+   * Copyright (c) 2011 Michael Frumin
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+   * use this file except in compliance with the License. You may obtain a copy of
+   * the License at
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+   * License for the specific language governing permissions and limitations under
+   * the License.
+   */
+
+?>
+<HTML>
+<HEAD>
+<TITLE>OpenStreetBlock Debugging Output for (<?=$lat?>,<?=$lon?>)</TITLE>
+</HEAD>
+<BODY>
 <H1>OpenStreetBlock Debugging Output</H1>
 
-<FORM>LAT: <input  name="lat" value="<?=$lat?>"> LON: <input type="text" name="lon" value="<?=$lon?>"> <input type="submit" name="GO"></FORM>
+<FORM>LAT: <input  name="lat" value="<?=$lat?>"> LON: <input type="text" name="lon" value="<?=$lon?>">
+<input type="hidden" name="format" value="debug">
+<input type="submit" name="GO">
+</FORM>
 
 <p>
 Closest OSM Way is: <strong><?= $osb[way][name] ?> (<?=$osb[way][osm_id] ?>)</strong> (Normalized: <?=street_norm($osb[way][name]);?>)
@@ -12,8 +39,11 @@ Closest OSM Way is: <strong><?= $osb[way][name] ?> (<?=$osb[way][osm_id] ?>)</st
 
 The <?= OSB_MAX_NODES_EXPAND ?> closest nodes are:
 
+FILL IN A BUNCH OF SHIT HERE
 
-
-   <p>Final Answer: <strong><?= osb_simple($osb); ?></strong></p>
+<p>Final Answer: <strong><?= osb_simple($osb); ?></strong></p>
 
 <iframe src="http://maps.google.com/maps?q=<?=$lat?>,<?=$lon?>" width="100%" height="500px">
+
+</BODY>
+</HTML>
--- index.php
+++ index.php
@@ -1,6 +1,45 @@
-holdover introductory page.  
+<?php
+
+  /**
+   * Copyright (c) 2011 Michael Frumin
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+   * use this file except in compliance with the License. You may obtain a copy of
+   * the License at
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+   * License for the specific language governing permissions and limitations under
+   * the License.
+   */
+
+?><HTML>
+<HEAD>
+<TITLE>OpenStreetBlock</TITLE>
+</HEAD>
+<BODY>
+<H1>What is OpenStreetBlock?</H1>
 
 Try <a href="osb.php?ll=40.6231433,-74.0282185">osb.php?ll=40.6231433,-74.0282185</a>
 <BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=json">osb.php?ll=40.6231433,-74.0282185&format=json</a>
 <BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=debug">osb.php?ll=40.6231433,-74.0282185&format=debug</a>
 <BR> or <a href="osb.php?ll=40.6231433,-74.0282185&format=rawdata">osb.php?ll=40.6231433,-74.0282185&format=rawdata</a>
+
+
+
+<H1>Is it Free?</H1>
+Yes!
+
+OpenStreetBlock is an <a href="https://github.com/fruminator/openstreetblock">open source</a> project,
+   first developed by <a href="http://frumin.net/ation/">Michael Frumin</a>.
+
+   OpenStreetBlock is published under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License, Version 2.0</a>.
+
+
+
+
+</BODY>
+</HEAD>
